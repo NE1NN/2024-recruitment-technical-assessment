239b09d05a8e886bd905f06b2fa966d69a46459d
Hindie <hindiesuputra@gmail.com>
2024-02-06 13:52:42 +1100

    change card from div to button

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,5 +1,3 @@
-import { StarIcon } from '@heroicons/react/24/solid';
-
 type CardProps = {
   coursePrefix: string;
   courseCode: number;
@@ -18,27 +16,29 @@ export default function Card({
   averageStars,
 }: CardProps) {
   return (
-    <div className="shadow-lg rounded py-5 px-3 bg-gray-50">
+    <button className="flex flex-col shadow-lg rounded-xl px-5 py-7 bg-gray-50">
       <div className="flex w-full justify-between">
         <div className="font-extrabold text-2xl">
           {coursePrefix}
           {courseCode}
         </div>
-        <div className='flex flex-col'>
+        <div className="flex flex-col items-start">
           <div className="flex">
             {Array.from({ length: 5 }).map((_, idx) => (
-              <StarIcon
-                key={idx}
-                className={`w-8 h-8 ${
-                  idx < averageStars ? 'fill-purple-500' : 'fill-gray-500'
+              <div
+                className={`text-2xl ${
+                  idx < averageStars ? 'text-purple-500' : 'text-gray-500'
                 }`}
-              />
+                key={idx}
+              >
+                ★
+              </div>
             ))}
           </div>
-          <div className="text-xs text-gray-500">{totalReviews} reviews</div>
+          <div className="text-xs text-gray-500 ml-1">{totalReviews} reviews</div>
         </div>
       </div>
-      <div className='text-sm mt-3'>{courseTitle}</div>
+      <div className="text-sm mt-3">{courseTitle}</div>
       <div className="flex gap-1 mt-10">
         {offeredTerms.map((term, idx) => (
           <div key={idx} className="bg-cyan-300 rounded-xl text-sm px-1">
@@ -46,6 +46,6 @@ export default function Card({
           </div>
         ))}
       </div>
-    </div>
+    </button>
   );
 }
