69248d64206712f044584963275e1e0c84326fc8
Hindie <hindiesuputra@gmail.com>
2024-02-07 12:16:30 +1100

    done

--- backend/ts/task.ts
+++ backend/ts/task.ts
@@ -56,13 +56,19 @@ type FileDataWithChildren = FileData & {
   children?: FileDataWithChildren[];
 };
 
-function calculateTotalSize(file: FileDataWithChildren): number {
+function calculateTotalSize(
+  fileId: number,
+  fileMap: Map<number, FileData & { children: FileData[] }>
+): number {
+  const file = fileMap.get(fileId);
+  if (!file) {
+    return 0;
+  }
   return (
     file.size +
-    (file.children?.reduce(
-      (acc, child) => acc + calculateTotalSize(child),
-      0
-    ) || 0)
+    file.children.reduce((acc, child) => {
+      return acc + calculateTotalSize(child.id, fileMap);
+    }, 0)
   );
 }
 
@@ -82,7 +88,7 @@ function largestFileSize(files: FileData[]): number {
   let maxSize = 0;
   fileMap.forEach((file) => {
     if (file.parent === -1) {
-      const currTotal = calculateTotalSize(file);
+      const currTotal = calculateTotalSize(file.id, fileMap);
       if (currTotal > maxSize) {
         maxSize = currTotal;
       }
@@ -189,6 +195,65 @@ console.assert(
   )
 );
 
+// Test 1.2: All files are leaf files
+console.assert(
+  arraysEqual(
+    leafFiles([
+      {
+        id: 1,
+        name: 'OnlyFile.txt',
+        categories: ['Only'],
+        parent: -1,
+        size: 500,
+      },
+      {
+        id: 3,
+        name: 'Document.txt',
+        categories: ['Document'],
+        parent: -1,
+        size: 300,
+      },
+      {
+        id: 9,
+        name: 'Video.mp4',
+        categories: ['Video'],
+        parent: -1,
+        size: 300,
+      },
+    ]),
+    ['OnlyFile.txt', 'Document.txt', 'Video.mp4']
+  ),
+  'Test 1.2 Failed'
+);
+
+// Test 1.3: Only 1 child
+console.assert(
+  arraysEqual(
+    leafFiles([
+      {
+        id: 1,
+        name: 'OnlyFile.txt',
+        categories: ['Only'],
+        parent: -1,
+        size: 500,
+      },
+      {
+        id: 3,
+        name: 'Document.txt',
+        categories: ['Document'],
+        parent: 1,
+        size: 300,
+      },
+    ]),
+    ['Document.txt']
+  ),
+  'Test 1.3 Failed'
+);
+
+// Test 1.4: Empty array
+console.assert(leafFiles([]).length === 0, 'Test 1.3 Failed');
+
+// Test 2
 console.assert(
   arraysEqual(kLargestCategories(testFiles, 3), [
     'Documents',
@@ -197,5 +262,118 @@ console.assert(
   ])
 );
 
+// Test 2.2: All files have equal size
+console.assert(
+  arraysEqual(
+    kLargestCategories(
+      [
+        {
+          id: 1,
+          name: 'OnlyFile.txt',
+          categories: ['B'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 2,
+          name: 'OnlyFile.txt',
+          categories: ['A'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 3,
+          name: 'OnlyFile.txt',
+          categories: ['C'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 4,
+          name: 'OnlyFile.txt',
+          categories: ['Z'],
+          parent: -1,
+          size: 500,
+        },
+      ],
+      4
+    ),
+    ['A', 'B', 'C', 'Z']
+  ),
+
+  'Test 2.2 Failed'
+);
+
+// Test 2.3: k is greater than the number of unique categories
+console.assert(
+  arraysEqual(
+    kLargestCategories(
+      [
+        {
+          id: 1,
+          name: 'OnlyFile.txt',
+          categories: ['B'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 2,
+          name: 'OnlyFile.txt',
+          categories: ['A'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 3,
+          name: 'OnlyFile.txt',
+          categories: ['C'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 4,
+          name: 'OnlyFile.txt',
+          categories: ['Z'],
+          parent: -1,
+          size: 500,
+        },
+        {
+          id: 5,
+          name: 'OnlyFile.txt',
+          categories: ['Z'],
+          parent: -1,
+          size: 500,
+        },
+      ],
+      10
+    ),
+    ['Z', 'A', 'B', 'C']
+  ),
+  'Test 2.3 Failed'
+);
+
+// Test 2.4: k is 0
+console.assert(
+  arraysEqual(kLargestCategories(testFiles, 0), []),
+  'Test 2.4 Failed'
+);
+
+// Test 2.5: Array is empty
+console.assert(arraysEqual(kLargestCategories([], 10), []), 'Test 2.5 Failed');
+
+// Test 3
 console.assert(largestFileSize(testFiles) == 20992);
-console.log(largestFileSize(testFiles));
+
+// Test 3.2: Single file with the largest size
+console.assert(
+  largestFileSize([
+    {
+      id: 1,
+      name: 'LargeFile.txt',
+      categories: ['Large'],
+      parent: -1,
+      size: 10000,
+    },
+  ]) === 10000,
+  'Test 3.2 Failed'
+);
